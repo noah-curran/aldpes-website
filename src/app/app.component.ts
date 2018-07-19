97c274931aa5fc021d582216b53de47cd8f03656
noah-curran <swimnoah113@gmail.com>
2018-07-18 20:25:00 -0700

    added some photos and made the gallery look pretty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,10 @@ export class AppComponent {
     
   constructor() {
     this.images = [];
-    this.images.push({source:'assets/images/test.jpeg', alt:'Description for Image 1', title:'Title 1'});
-    this.images.push({source: 'assets/images/Nature-PNG-Clipart-Image-02.png', alt: '234', title: 'title 2'})
+    this.images.push({source:'assets/images/arch.jpg', alt:'Take a picture under the archway.', title:'Welcome to Purdue!'});
+    this.images.push({source: 'assets/images/fountain.jpg', alt: 'Run through the fountain!', title: 'Loeb Fountain'});
+    this.images.push({source: 'assets/images/flag.jpg', alt: 'Go boilermakers!', title: 'Boiler Up!'});
+    this.images.push({source: 'assets/images/dunk.jpg', alt: 'Purdue dunks on another team.', title: 'Hammer Down!'});
+    this.images.push({source: 'assets/images/walc.jpg', alt: 'Study hard.', title: 'Hail Purdue.'});
   }
 }
